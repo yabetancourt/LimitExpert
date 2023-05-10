eea03235f37c85ed5f0c79fb6086091dd42b7a01
yabetancourt <yabetancourt@uclv.cu>
2023-05-10 14:44:07 -0400

    Se agregan las dependencias necesarias para conectarse con prolog

--- src/main/java/cu/limitexpert/Application.java
+++ src/main/java/cu/limitexpert/Application.java
@@ -3,7 +3,10 @@ package cu.limitexpert;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 /**
  * The entry point of the Spring Boot application.
@@ -13,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 @Theme(value = "expertoenlmites")
 public class Application implements AppShellConfigurator {
 
