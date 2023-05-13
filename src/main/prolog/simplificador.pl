1da5e8b68c86823cecfe66eea581b8d60efde99e
Marlin <ramirezmarlin174@gmail.com>
2023-05-13 12:22:24 -0400

    Se modifico la potencia en el simplificador

--- src/main/prolog/simplificador.pl
+++ src/main/prolog/simplificador.pl
@@ -13,43 +13,9 @@ simplificar(F, S) :-
 simplificar(X, X).
 
 % Reglas para simplificar operaciones
-simplificar_oper(+, A, B+C, R+C) :-
-    (number(A), number(B), R is A + B, !);
-    simplificar_oper(+, A, B, R).
-
-simplificar_oper(+, A+B, C, R+B) :-
-    (number(A), number(C), R is A + C, !);
-    simplificar_oper(+, A, C, R).
-
 simplificar_oper(+, 0, B, B).
 
 simplificar_oper(+, A, 0, A).
-
-simplificar_oper(+, X, X, 2*X).
-
-simplificar_oper(+, N*X, K*X, S) :-
-    (number(N), number(K), Sum is N + K, S = Sum*X, !);
-    (S = (N + K) * X, !).
-
-simplificar_oper(+, X, K*X, S) :-
-    (number(K), Sum is 1 + K, S = Sum*X, !);
-    (S = (1 + K) * X, !).
-
-simplificar_oper(+, N*X, X, S) :-
-    (number(N), Sum is 1 + N, S = Sum*X, !);
-    (S = (1 + N) * X, !).
-
-simplificar_oper(-, N*X, K*X, S) :-
-    (number(N), number(K), Sum is N - K, S = Sum*X, !);
-    (S = (N - K) * X, !).
-
-simplificar_oper(-, X, K*X, S) :-
-    (number(K), Sum is 1 - K, S = Sum*X, !);
-    (S = (1 - K) * X, !).
-
-simplificar_oper(-, N*X, X, S) :-
-    (number(N), Sum is N - 1, S = Sum*X, !);
-    (S = (N - 1) * X, !).
 
 simplificar_oper(+, A, B, S) :-
     (number(B), number(A), S is A + B , !);
@@ -83,16 +49,11 @@ simplificar_oper(/, A, B, S) :-
     number(SB), number(SA), S is SA / SB;
     S =.. [/, SA, SB]), !.
 
-simplificar_oper(^, _, 0, 1) :- !.
-
-simplificar_oper(^, A, 1, S) :-
-    simplificar(A, S), !.
+simplificar_oper(^, _, 0, 1).
+simplificar_oper(^, A, 1, A).
+simplificar_oper(^, 0, _, 0).
+simplificar_oper(^, 1, _, 1).
 
 simplificar_oper(^, A, B, S) :-
-    simplificar(A, SA),
-    simplificar(B, SB),
-    (SA = 0, S = 0;
-     SB = 0, S = 1;
-     SA = 1, S = 1;
-     number(SA), number(SB), S is SA^SB;
-     S =.. [^, SA, SB]), !.
+     (number(A), number(B), S is A^B , !);
+     (S =.. [^, A, B]), !.
