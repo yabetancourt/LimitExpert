ac4493f8747279459388c7a9c674beff2ace00a2
Daly <dalianacabrera@icloud.com>
2023-05-28 10:39:36 -0500

    Arreglar los pasos

--- src/main/prolog/limite.pl
+++ src/main/prolog/limite.pl
@@ -1,5 +1,5 @@
+:- set_prolog_flag(answer_write_options, [max_depth(1000)]).
 :- use_module(derivador, [derivar/4]).
-
 %indeterminaciones%
 indeterm(X*infinito,_,_,indeterminado):-X=:=0.
 indeterm(infinito*X,_,_,indeterminado):-X=:=0.
@@ -12,18 +12,18 @@ indeterm(infinito-infinito,_,_,indeterminado).
 %limites bases%
 limite(e,_,_,2.7183,[('Constante de Euler',2.7183)]).
 
-limite(X,X,P,P,[('Limite de una variable que se aproxima a un punto',P)]).
+limite(X,X,P,P,[('Límite de una variable que se aproxima a un punto',P)]).
 
-limite(X,_,_,L,[('Limite de una constante',X)]):-
+limite(X,_,_,L,[('Límite de una constante',X)]):-
 	number(X),
 	L is X.
 limite(-X,V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	L= -LX,
-	append(Pasos1,[('Limite de una expresion negativa',L)],Pasos).
+	append(Pasos1,[('Límite de una expresión negativa',L)],Pasos).
 
 %limite fundamental algebraico%
-limite((1+1/X)**X,X,infinito,e,[('Limite fundamental algebraico',e)]).
+limite((1+1/X)**X,X,infinito,e,[('Límite fundamental algebraico',e)]).
 
 %limite del producto%
 limite(X*Y,V,P,L,Pasos):-
@@ -66,62 +66,62 @@ limite(X^Y,V,P,L,Pasos):-
 limite(sen(X),V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	((atom(LX),L=LX);(LT is sin(LX),aproximar(LT,L))),!,
-	append(Pasos1,[('Limite del seno',L)],Pasos).
+	append(Pasos1,[('Límite del seno',L)],Pasos).
 
 limite(cos(X),V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	((atom(LX),L=LX);(LT is cos(LX),aproximar(LT,L))),!,
-	append(Pasos1,[('Limite del coseno',L)],Pasos).
+	append(Pasos1,[('Límite del coseno',L)],Pasos).
 
 
 limite(tan(X),V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	((atom(LX),L=LX);(LT is tan(LX),aproximar(LT,L))),!,
-	append(Pasos1,[('Limite de la tangente',L)],Pasos).
+	append(Pasos1,[('Límite de la tangente',L)],Pasos).
 
 
 limite(asen(X),V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	((atom(LX),L=LX);(LT is asin(LX),aproximar(LT,L))),!,
-	append(Pasos1,[('Limite del arcoseno',L)],Pasos).
+	append(Pasos1,[('Límite del arcoseno',L)],Pasos).
 
 
 limite(acos(X),V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	((atom(LX),L=LX);(LT is acos(LX),aproximar(LT,L))),!,
-	append(Pasos1,[('Limite del arcocoseno',L)],Pasos).
+	append(Pasos1,[('Límite del arcocoseno',L)],Pasos).
 
 
 limite(atan(X),V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	((atom(LX),L=LX);(LT is atan(LX),aproximar(LT,L))),!,
-	append(Pasos1,[('Limite del arcotangente',L)],Pasos).
+	append(Pasos1,[('Límite del arcotangente',L)],Pasos).
 
 
 %Limite del modulo%
 limite(abs(X),V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	((atom(LX),L=LX);(L is abs(LX))),!,
-	append(Pasos1,[('Limite del módulo',L)],Pasos).
+	append(Pasos1,[('Límite del módulo',L)],Pasos).
 
 
 %Limite con raiz cuadrada%
 limite(sqrt(X),V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	((atom(LX),L=LX);(LA is sqrt(LX),aproximar(LA,L))),!,
-	append(Pasos1,[('Limite de la raíz cuadrada',L)],Pasos).
+	append(Pasos1,[('Límite de la raíz cuadrada',L)],Pasos).
 
 
 %limite con logaritmos base 10 y log natural%
 limite(ln(X),V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	((LX=<0,L=indeterminado);(LA is log(LX),aproximar(LA,L))),!,
-	append(Pasos1,[('Limite del logaritmo natural',L)],Pasos).
+	append(Pasos1,[('Límite del logaritmo natural',L)],Pasos).
 
 limite(log(X),V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	((LX=<0,L=indeterminado);(LA is log10(LX),aproximar(LA,L))),!,
-	append(Pasos1,[('Limite del logaritmo base 10',L)],Pasos).
+	append(Pasos1,[('Límite del logaritmo base 10',L)],Pasos).
 
 
 %otros%
@@ -132,8 +132,12 @@ limite_zero(X/Y,V,P,L,Pasos):-
 limite_div(X/Y,V,P,L,Pasos):-
 	limite(X,V,P,LX,Pasos1),
 	limite(Y,V,P,LY,Pasos2),
-        ((indeterm(LX/LY,_,_,_),limite_LH(X/Y,V,P,L));(number(LX),LY=infinito,L=0);(number(LX),LY=:=0,L = infinito);(atom(LX),L = LX);(atom(LY),L = LY);(mcd(LX,LY,D),XD is LX/D,YD is LY/D,((YD=:=1,L=XD);L=XD/YD))),!,
-	append(Pasos1,Pasos2,Aux),append(Aux,[('Limite de la división',L)],Pasos).
+        ((indeterm(LX/LY,_,_,_),limite_LH(X/Y,V,P,L),append(Pasos1,Pasos2,Aux),append(Aux,['Límite indeterminado',('Aplicando LHospital',L)],Pasos));
+	(number(LX),LY=infinito,L=0,append(Pasos1,Pasos2,Aux),append(Aux,[('Límite de la división',L)],Pasos));
+	(number(LX),LY=:=0,L = infinito,append(Pasos1,Pasos2,Aux),append(Aux,[('Límite de la división',L)],Pasos));
+	(atom(LX),L = LX,append(Pasos1,Pasos2,Aux),append(Aux,[('Límite de la división',L)],Pasos));
+	(atom(LY),L = LY,append(Pasos1,Pasos2,Aux),append(Aux,[('Límite de la división',L)],Pasos));
+	(mcd(LX,LY,D),XD is LX/D,YD is LY/D,((YD=:=1,L=XD);L=XD/YD),!,append(Pasos1,Pasos2,Aux),append(Aux,[('Hallando mcd y dividiendo',L)],Pasos))),!.
 
 aproximar(X,A):-
 	format(atom(NA),'~1f',X),
